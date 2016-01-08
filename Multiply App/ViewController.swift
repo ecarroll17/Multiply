27ef3e313909696fa0658dfac8b284ab9ecae8fe
ecarroll <student@ecarroll.local>
2016-01-08 11:16:11 -0600

    initial commit

--- Multiply App/ViewController.swift
+++ Multiply App/ViewController.swift
@@ -8,16 +8,90 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var segCont: UISegmentedControl!
+    
+    @IBAction func chosenOperator(sender: UISegmentedControl)
+    {
+        let firstNumberString = textField1.text
+        let firstNumberDouble = (firstNumberString! as NSString).doubleValue
+        
+        let secondNumberString = textField2.text
+        let secondNumberDouble = (secondNumberString! as NSString).doubleValue
+
+        if segCont.selectedSegmentIndex == 0
+        {
+            var ansOne = firstNumberDouble + secondNumberDouble
+            
+            answerLabel.text = "\(ansOne)"
+            
+            myLabel.text = "+"
+            
+            myImage.image = UIImage(named: "white")
+        }
+       
+        if segCont.selectedSegmentIndex == 1
+        {
+            var ansTwo = firstNumberDouble - secondNumberDouble
+            
+            answerLabel.text = "\(ansTwo)"
+            
+            myLabel.text = "-"
+            
+            myImage.image = UIImage(named: "white")
+        }
+        
+        if segCont.selectedSegmentIndex == 2
+        {
+            var ansThree = firstNumberDouble * secondNumberDouble
+            
+            answerLabel.text = "\(ansThree)"
+            
+            myLabel.text = "X"
+            
+            if (ansThree % 2) == 0
+            {
+                myImage.image = UIImage(named: "funnyImage")
+            }
+            
+            else
+            {
+                myImage.image = UIImage(named: "funnierImage")
+            }
+            
+            if ansThree == 64
+            {
+                myImage.image = UIImage(named: "toadMarioKart")
+            }
+        }
+        
+        if segCont.selectedSegmentIndex == 3
+        {
+            var ansFour = firstNumberDouble / secondNumberDouble
+            
+            answerLabel.text = "\(ansFour)"
+            
+            myLabel.text = "/"
+            
+            myImage.image = UIImage(named: "white")
+        }
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
